45d6dad24bd5799161ab9c56fe671aad830b144e
Valeriya <valeriya-fursenko@yandex.ru>
2015-10-26 15:16:36 +0100

    Second changes

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -17,7 +17,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello ASE2015 - how are you? :)");
+		System.out.println("Hello ASE2015 - how are you now? :)");
 	}
 
 }
